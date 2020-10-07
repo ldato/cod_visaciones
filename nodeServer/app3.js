7d6234ad1c62a1229d61c5a993f4ca49a7755c31
Luciano Dato <lucianodato82@gmail.com>
2020-10-07 12:10:06 -0300

    Funcionando correctamente apuntando al 33

--- nodeServer/app3.js
+++ nodeServer/app3.js
@@ -42,7 +42,7 @@ app.get("/facturas", function(req , res){
   dbConn.connect().then(async function () {
       var request = new sql.Request(dbConn);
       await request.query(`INSERT INTO CONT_VISAC.dbo.Trx_Visac1 (NroFact, Fecha, Nombre, CantCert, ImpTotal)
-      SELECT  C.VTRMVH_NROFOR, FORMAT(VTRMVH_FECMOD, 'dd/MM/yyyy HH:mm:ss')AS FECHA , E.VTMCLH_NOMBRE,
+      SELECT  C.VTRMVH_NROFOR, VTRMVH_FECMOD, E.VTMCLH_NOMBRE,
       CAST(SUM(VTRMVI_CANTID) AS INT) AS cantXprod,
       CAST(SUM (VTRMVI_PRENAC * VTRMVI_CANTID) AS INT) AS totXprod
             FROM VTRMVI A
@@ -75,7 +75,7 @@ dbConn.connect().then(function () {
     request.query(`select top 20 NroFact, FORMAT(Fecha, 'dd/MM/yyyy HH:mm:ss') as Fecha , Nombre, CantCert, ImpTotal, Estado from (select *,
       row_number() over (partition by Nrofact order by Fecha desc) as rn
       from CONT_VISAC.dbo.Trx_Visac1) t
-      where t.rn = 1 order by Fecha desc;`).then(function (resp) {
+      where t.rn = 1 order by NroFact desc;`).then(function (resp) {
         console.log(resp.recordset);
         res.send(resp.recordset);
         dbConn.close();
